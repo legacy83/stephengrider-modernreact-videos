d4f71e954b8138af8494d1d68aa1748bd1c6b99d
Thiago Senna <trsenna@gmail.com>
2021-05-27 19:50:26 -0300

    now searching from youtube

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import SearchBar from './components/SearchBar';
 import youtube from './apis/youtube';
 
 class App extends React.Component {
-	onTermSubmit = (term) => {
-		youtube('/search', {
+	state = { videos: [] };
+
+	onTermSubmit = async (term) => {
+		const response = await youtube('/search', {
 			params: {
 				q: term,
 			},
 		});
+
+		this.setState({ videos: response.data.items });
 	};
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<p>I have a total of {this.state.videos.length} video(s).</p>
 			</div>
 		);
 	}
