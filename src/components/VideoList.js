ab161c89800857d8b3eb2861759ff48b06f4a4bc
Thiago Senna <trsenna@gmail.com>
2021-05-27 20:17:56 -0300

    some styles added

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,12 +3,12 @@ import VideoListItem from './VideoListItem';
 
 const VideoList = ({ videos }) => {
 	const videoListItems = videos.map((video) => {
-		return <VideoListItem />;
+		return <VideoListItem video={video} />;
 	});
 
 	return (
 		<div>
-			{videoListItems}
+			<div className="ui relaxed divided list">{videoListItems}</div>
 			<p>Listing a total of {videos.length} video(s).</p>
 		</div>
 	);
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './VideoListItem.css';
 
-const VideoListItem = (props) => {
-	return <div>video list item</div>;
+const VideoListItem = ({ video }) => {
+	return (
+		<div className="item item--video">
+			<img className="ui image" src={video.snippet.thumbnails.medium.url} />
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+			</div>
+		</div>
+	);
 };
 
 export default VideoListItem;
